3c02eedf53c184a448616c195b0422094961a58e
brian wanjala <brianwanja302@gmail.com>
2026-02-24 12:26:09 +0300

    added superuser

--- config/settings.py
+++ config/settings.py
@@ -128,3 +128,13 @@ MEDIA_ROOT = BASE_DIR/'media'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 DEFAULT_FROM_EMAIL = "watalii@bookings.com"
+
+if os.environ.get("DJANGO_SUPERUSER_USERNAME"):
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username=os.environ.get("DJANGO_SUPERUSER_USERNAME")).exists():
+        User.objects.create_superuser(
+            os.environ.get("DJANGO_SUPERUSER_USERNAME"),
+            os.environ.get("DJANGO_SUPERUSER_EMAIL"),
+            os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+        )
